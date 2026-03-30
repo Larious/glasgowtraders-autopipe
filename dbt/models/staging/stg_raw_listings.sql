d1955c25f3e0266812922f12886108e494131179
Abraham Damola Aroloye <133163066+Larious@users.noreply.github.com>
2026-03-30 20:53:26 +0100

    phase-04: dbt models, enrichment, and schema tests

---
+++ dbt/models/staging/stg_raw_listings.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='view') }}
+
+SELECT
+    place_id, name, address, phone, website,
+    postcode, latitude, longitude, categories,
+    opening_hours, rating, review_count,
+    business_status, pipeline_status,
+    content_hash, wp_post_id, scraped_at
+FROM staging.raw_listings
+WHERE pipeline_status IN ('STAGED', 'TRANSFORMED')
+```
+
+Save. Then:
+```
+nano dbt/models/intermediate/int_listings_enriched.sql
